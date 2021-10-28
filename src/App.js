4190e67e5ff85726037c72676e9be354f7a50f3f
fmoyaj <60526175+fmoyaj@users.noreply.github.com>
2021-10-27 20:50:25 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var MAX_EVENTS = 3;
+var MAX_COINEM_PER_EVENT = 5;
+var MAX_COINEM = 5;
+var NEXT_EVENT_UID = 1;
+
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      members : [],
+
+
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <header>Join'Em</header>
+      </div>
+    );
+  }
+
 }
+
+/**
+ * ErrorBoundaries can improve error reporting. This class is copied from:
+ * https://reactjs.org/docs/error-boundaries.html
+ */
+ class ErrorBoundary extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {    
+    // Update state so the next render will show the fallback UI.    
+    return { hasError: true };
+  }
+  
+  componentDidCatch(error, errorInfo) {    
+    // You can also log the error to an error reporting service    
+    console.log(`error: ${error}; errorInfo: ${errorInfo}`);  
+  }
+ 
+  render() {
+    if (this.state.hasError) {      
+      // You can render any custom fallback UI      
+      return <h1>Something went wrong.</h1>;    
+    }
+    return this.props.children; 
+  }
+}
+
+
 
 export default App;
